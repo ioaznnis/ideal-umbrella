77ca5cb01595f1348477e56841fb5c6ff66c1a30
Ivan Anzin <1431061+ioaznnis@users.noreply.github.com>
2022-07-31 12:24:24 +0300

    (main) Add code to reproduce the exception

---
+++ NpgsqlRangeError/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.EntityFrameworkCore;
+using NpgsqlTypes;
+
+Console.WriteLine("Hello, World!");
+
+public class Entity
+{
+    public Guid Id { get; set; }
+    
+    public NpgsqlRange<decimal> Range { get; set; }
+}
+
+public class ExampleDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("connectionString");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Entity>().HasIndex(entity => entity.Range).IsUnique();
+    }
+}
